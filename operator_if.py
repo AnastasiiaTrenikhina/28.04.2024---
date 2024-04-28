974864ac633ada09d9b05972b553840a3e260ec3
AnastasiiaTrenikhina <167620693+AnastasiiaTrenikhina@users.noreply.github.com>
2024-04-28 16:50:12 +0300

    Add files via upload

---
+++ operator_if.py
@@ -0,0 +1,32 @@
+x = 38
+print('дратути!')
+if x < 0:
+    print('Меньше нуля')
+print('До свидания')
+
+
+a,b = 10,5
+if  a> b:
+    print('a>b')
+if a > b and a>0:
+    print('успех')
+if (a>b) and (a > 0 or b<1000):
+    print('успех')
+if 5 < b and b<10:
+    print('успех')
+
+ #if '34' > '123':   #на этом этапе ошибка :unindent does not match any outer indentation level
+    #print('успех')
+
+# if '123' > '12':
+    # print('успех') #на этом этапе ошибка :unindent does not match any outer indentation level
+
+#if [1,2] > [1,1]:
+   #print('успех') #на этом этапе ошибка :unindent does not match any outer indentation level
+
+ #if '6' > 5:
+     #print('успех')
+ if [5,6]' > 5:  # ошибка  этом этапе ошибка :unindent does not match any outer indentation level
+print('успех')
+if '6' != 5 :
+   print('успех')
